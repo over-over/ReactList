c9f71c44b7f65462486346a74ed78b6006ff2ead
Overdoze <klg.panov@gmail.com>
2019-10-28 04:48:54 +0300

    build folder

---
+++ precache-manifest.785468cb1e2c6d87074554662337954c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9303b03654f0e7463759d44cd50f347a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dd2794f7f4f4f436fa2",
+    "url": "/static/css/main.05eac79d.chunk.css"
+  },
+  {
+    "revision": "1d099576ec08ebc22ada",
+    "url": "/static/js/2.751caeca.chunk.js"
+  },
+  {
+    "revision": "9dd2794f7f4f4f436fa2",
+    "url": "/static/js/main.8cde57d8.chunk.js"
+  },
+  {
+    "revision": "49ef00588cebed032498",
+    "url": "/static/js/runtime-main.4698c9b2.js"
+  }
+]);
